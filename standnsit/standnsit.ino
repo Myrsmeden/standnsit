e865b462ec9f04dc3a561d20aef742d0d1b84d55
Johan Myrsmeden <johan@myrsmeden.se>
2017-05-14 21:08:59 +0200

    Updated documentation

--- standnsit/standnsit.ino
+++ standnsit/standnsit.ino
@@ -1,22 +1,41 @@
-int led1 = 11;
-int led2 = 12;
-#define trigPin 9         // Pin for trig signal
-#define echoPin 8         // Pin for echo signal
-#define sitHeight 70      // Height of table when sitting [cm]
-#define standHeight 95   // Height of table when standing [cm]
-#define touchSensor 10
+/*
+ * 
+ * Code for Stand'n'sit application
+ * An utility to remind you it is time to change working position
+ * @author Johan Myrsmeden
+ * GitHub: @myrsmeden
+ * 
+ */
 
+// Define the purpose of the GPIO pins
+#define led0 11         // First led pin, bit 0
+#define led1 = 12       // Second led pin, bit 1
+#define trigPin 9       // Pin for trig signal for distance sensor
+#define echoPin 8       // Pin for echo signal for distance sensor
+#define sitHeight 70    // Height of table when sitting [cm]
+#define standHeight 95  // Height of table when standing [cm]
+#define touchSensor 10  // Pin for the out signal of the touch sensor
+
+// Define the different modes that you can have
+// It is an array of four tuples, where 
+// the first value in each tuple is the time you should be sitting and 
+// the second value is the time you should be standing
+int modes[][4] = {{20*60,20*60},{20*60,20*60},{20*60,20*60},{20*60,20*60}};
+
+// Variable to remember which mode you are in
 int mode = 0;
-int modes[][2] = {{20*60,20*60},{20*60,20*60},{20*60,20*60},{20*60,20*60}};
 
+// Boolean for standing or sitting
 int standing = 0;
+
+// The timer, our counter variable
 int timer = 0;
 
-int setting = 0;
-
+// Setup function
+// Set output and input pins and set the timer
 void setup() {
+  pinMode(led0, OUTPUT);
   pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -25,6 +44,7 @@ void setup() {
   Serial.println("Setting up");
 }
 
+// Function to get the distance to the floor from the sensor
 int getDistance() {
   long duration, distance;
   digitalWrite(trigPin, LOW);         // Send LOW signal to ultrasonic sensor
@@ -39,13 +59,16 @@ int getDistance() {
   return distance;
 }
 
+// Function to blink with leds
 void blink() {
-  digitalWrite(led1, HIGH);
+  digitalWrite(led0, HIGH);
   delay(100);
-  digitalWrite(led1, LOW);
+  digitalWrite(led0, LOW);
   delay(100);
 }
 
+// The alarm function
+// Blink as long as the distance is not correct
 void alarm() {
   if(standing) {
       while(getDistance() < standHeight) {
@@ -58,21 +81,25 @@ void alarm() {
     }
 }
 
+// Function to light the leds to show the (binary) value
+// stored in mode, to tell the user which mode that is active
 void lightLeds(int mode) {
-  Serial.println(mode);
+  // The least significant bit
   if ( mode % 2 == 1 ) {
+    digitalWrite(led0, HIGH);
+  } else {
+    digitalWrite(led0, LOW);
+  }
+  // The most significant bit
+  if ( mode > 1 ) {
     digitalWrite(led1, HIGH);
   } else {
     digitalWrite(led1, LOW);
   }
-
-  if ( mode > 1 ) {
-    digitalWrite(led2, HIGH);
-  } else {
-    digitalWrite(led2, LOW);
-  }
 }
 
+// Function to ping the touch sensor 
+// and change the mode variable and update the leds
 void checkModeChange() {
   if (digitalRead(touchSensor) == HIGH) {
     mode++;
@@ -87,6 +114,12 @@ void checkModeChange() {
   
 }
 
+// Our loop function
+// Decrease the timer until it reaches zero
+// Then change standing boolean and update the timer
+// NOTE: For now, the timer variable only changes when it reaches zero, 
+// you cannot change the timer when running
+// It is a temporary solution to prevent cheating
 void loop() {
   while(timer > 0) {
     checkModeChange();
